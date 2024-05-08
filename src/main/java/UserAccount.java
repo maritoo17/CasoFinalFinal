21f38a011759b3e1c2b5751582fa9262274d598c
maritoo17 <msancgim@myuax.com>
2024-05-08 10:10:40 +0200

    16

--- src/main/java/UserAccount.java
+++ src/main/java/UserAccount.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserAccount {
     public String alias;
@@ -61,11 +62,34 @@ public class UserAccount {
 }
 
 class Utils {
-    public static boolean isValidEmail(String email) {
-        return email.contains("@");
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String leerCadena() {
+        return scanner.nextLine();
     }
 
-    public static boolean isValidAlias(String alias) {
-        return alias.matches("[a-zA-Z0-9]+");
+    public static int leerEntero() {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Por favor, introduce un número entero.");
+        }
+        return scanner.nextInt();
+    }
+
+    public static float leerFloat() {
+        while (!scanner.hasNextFloat()) {
+            scanner.next();
+            System.out.println("Por favor, introduce un número flotante.");
+        }
+        return scanner.nextFloat();
+    }
+
+    public static double leerDouble() {
+        while (!scanner.hasNextDouble()) {
+            scanner.next();
+            System.out.println("Por favor, introduce un número doble.");
+        }
+        return scanner.nextDouble();
     }
 }
+
